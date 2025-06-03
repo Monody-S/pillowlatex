9d79eae29b8ef5c1868e7a5ff004ac447da70a0d
迷梦龙 <614675349@qq.com>
2025-06-03 18:37:13 +0800

    初始化

---
+++ src/pillowlatex/__init__.py
@@ -0,0 +1,48 @@
+from .latex import (
+    LaTexImage,
+    LaTexFunc,
+    LaTexEnvFunc,
+    LaTexImageDraw,
+    
+    middle_lowandpows,
+    auto_middle_replaces,
+    big_replaces,
+    high_replaces,
+    replaces,
+
+    RenderLaTex,
+    RenderLaTexObjs,
+    GetLaTexObjs,
+
+    RegisterLaTexEnvFunc,
+    RegisterLaTexFunc
+)
+
+from . import settings
+
+from .latexenvfuncs import *
+from .latexfuncs import *
+
+__version__ = "0.1.0"
+__all__ = [
+    "LaTexImage",
+    "LaTexFunc",
+    "LaTexEnvFunc",
+    "LaTexImageDraw",
+    
+    "middle_lowandpows",
+    "auto_middle_replaces",
+    "big_replaces",
+    "high_replaces",
+    "replaces",
+
+    "RenderLaTex",
+    "RenderLaTexObjs",
+    "GetLaTexObjs",
+
+    "RegisterLaTexEnvFunc",
+    "RegisterLaTexFunc",
+
+    # Settings
+    "settings"
+]
